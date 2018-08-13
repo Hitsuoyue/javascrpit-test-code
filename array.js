340f7058e9462e0e10bea63f05ab7e2b25cfec8f
suoyue <1255915270@qq.com>
2018-08-13 18:06:55 +0800

    111

--- array.js
+++ array.js
@@ -5,12 +5,12 @@
 //         return true
 //     }
 // }));
-let arr = [1,2,3,4,5,6,7];
-arr.sort(function(a,b){
-    // return a-b;//从小到大排序
-    return b-a;//从大到小排序
-})
-console.log(arr);
+// let arr = [1,2,3,4,5,6,7];
+// arr.sort(function(a,b){
+//     // return a-b;//从小到大排序
+//     return b-a;//从大到小排序
+// })
+// console.log(arr);
 
 
 // let arr1 = arr.map(function(item){
@@ -45,3 +45,66 @@ console.log(arr);
 // console.log(date.getHours())
 // console.log(date.getMinutes())
 // console.log(date.getSeconds())
+
+//写一个能遍历对象和数组的 forEach 函数
+// function forEach(obj, fn) {
+//     if(Array.isArray(obj)){
+//         obj.forEach(function (item, index) {
+//             fn(item, index)
+//         })
+//
+//     }else{
+//         for(key in obj){
+//             let item = obj[key]
+//             fn(item, key);
+//         }
+//
+//     }
+// }
+//
+// let arr = [1,2,3,4,5];
+// let obj = {name: 111, age: 222}
+//
+// forEach(arr, function (item, index) {
+//     console.log(item, index)
+// })
+// forEach(obj, function (item, index) {
+//     console.log(item, index)
+// })
+
+//获取一个 2018-8-13 格式的日期
+function formatDate(date, mark) {
+    if(!date){
+        date = new Date();
+    }
+    let year = date.getFullYear();
+    let month = date.getMonth()+1;
+    if(month < 10){
+        month = `0${month}`
+    }
+    let day = date.getDate();
+    if(day < 10){
+        day = `0${day}`
+    }
+    return `${year}${mark}${month}${mark}${day}`
+}
+console.log(formatDate(new Date(), '/'))
+
+
+
+function deepClone(initalObj) {
+    var obj = {};
+    obj = JSON.parse(JSON.stringify(initalObj));
+    return obj;
+}
+
+var obj = {
+    a: {
+        a: "world",
+        b: 21
+    }
+}
+var cloneObj = deepClone(obj);
+cloneObj.a.a = "changed";
+
+console.log(obj.a.a); // "world"
